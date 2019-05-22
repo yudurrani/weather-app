cebc450ac04e7a121ca1f35d252df3123bfa74a9
Yasir Durrani <yudurrani@gmail.com>
2019-05-21 21:51:43 -0400

    finding the location

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-console.log('Starting')
+const request = require('request')
 
 
-setTimeout(() => {
+const url = 'https://api.darksky.net/forecast/8f749f307f69ae04f74c491ce934b590/37.8267,-122.4233'
 
-console.log('2 sec wait')
-}, 2000)
+request({url: url, json: true},(error, response) => {
+    if(error){
+        console.log('Cannot connect to the weather app')
 
-setTimeout( () => {
-    console.log('0 sec wait')
-}, 0)
+    }else if(response.body.error){
+        console.log('Cannot find the location of subject')
+
+    
+    }else {
+
+    
+   console.log( response.body.daily.data[0].summary + ' It is currently ' , response.body.currently.temperature , ' degrees out. There is a ', response.body.currently.precipProbability , ' percent chance of rain' )
+    }
+    //    const data = JSON.parse(response.body)
+    //    console.log(data.currently)
+    })
+
+const geoCodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaXJ0aGVkdXJyYW5pIiwiYSI6ImNqdnhoMXA5azA1MmY0YW1qaDRjZjluMnIifQ.JC-I3GJHfksjPee6Q_19IA&limit=1'
 
 
+request({url:geoCodeurl, json: true},(error, response) => {
 
-console.log('stopping')
+    if(error){
+        console.log('Cannot connect to the locator')
+    }else if(response.body.features.length === 0){
+        console.log('Cannot find the location')
+    }else{
+
+    const longitude = response.body.features[0].center[0]
+    const latitude = response.body.features[0].center[1]
+
+    console.log( 'Latitude = ' + latitude + ' Longitude = ' + longitude)
+    }
+   
+    
+})
