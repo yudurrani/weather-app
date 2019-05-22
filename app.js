7b1c42d8d204f9e9dcc9089be221c13f35a69df7
Yasir Durrani <yudurrani@gmail.com>
2019-05-22 07:30:49 -0400

    Making geoCode

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// const request = require('request')
+ //import {request} from "https";
+
+ const request = require('request')
 
 
 // const url = 'https://api.darksky.net/forecast/8f749f307f69ae04f74c491ce934b590/37.8267,-122.4233'
@@ -16,8 +18,7 @@
     
 //    console.log( response.body.daily.data[0].summary + ' It is currently ' , response.body.currently.temperature , ' degrees out. There is a ', response.body.currently.precipProbability , ' percent chance of rain' )
 //     }
-//     //    const data = JSON.parse(response.body)
-//     //    console.log(data.currently)
+    
 //     })
 
 // const geoCodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaXJ0aGVkdXJyYW5pIiwiYSI6ImNqdnhoMXA5azA1MmY0YW1qaDRjZjluMnIifQ.JC-I3GJHfksjPee6Q_19IA&limit=1'
@@ -39,3 +40,27 @@
    
     
 // })
+
+const geocode = (address, callback) => {
+
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoieWFzaXJ0aGVkdXJyYW5pIiwiYSI6ImNqdnhoMXA5azA1MmY0YW1qaDRjZjluMnIifQ.JC-I3GJHfksjPee6Q_19IA&limit=1'
+
+    request({url: url, json: true} , (error, response) => {
+       
+        if(error) {
+            callback('Unable to connect to location services' , undefined)
+        }else if(response.body.features.length === 0){
+            callback('Unable to connect to location. Try aother search' , undefined)
+        }else{
+            
+        }
+
+    })
+}
+
+geocode( '12what', (error, data) => {
+
+    console.log('Error' , error)
+    console.log('Data' , data)
+
+})
