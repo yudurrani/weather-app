1162c6f32fb906123da2a0ed14b9c9b06e8ff638
Yasir Durrani <yudurrani@gmail.com>
2019-05-22 01:05:57 -0400

    practicing the callbacks

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const request = require('request')
+// const request = require('request')
 
 
-const url = 'https://api.darksky.net/forecast/8f749f307f69ae04f74c491ce934b590/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/8f749f307f69ae04f74c491ce934b590/37.8267,-122.4233'
 
-request({url: url, json: true},(error, response) => {
-    if(error){
-        console.log('Cannot connect to the weather app')
+// request({url: url, json: true},(error, response) => {
+//     if(error){
+//         console.log('Cannot connect to the weather app')
 
-    }else if(response.body.error){
-        console.log('Cannot find the location of subject')
-
-    
-    }else {
+//     }else if(response.body.error){
+//         console.log('Cannot find the location of subject')
 
     
-   console.log( response.body.daily.data[0].summary + ' It is currently ' , response.body.currently.temperature , ' degrees out. There is a ', response.body.currently.precipProbability , ' percent chance of rain' )
-    }
-    //    const data = JSON.parse(response.body)
-    //    console.log(data.currently)
-    })
+//     }else {
 
-const geoCodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaXJ0aGVkdXJyYW5pIiwiYSI6ImNqdnhoMXA5azA1MmY0YW1qaDRjZjluMnIifQ.JC-I3GJHfksjPee6Q_19IA&limit=1'
+    
+//    console.log( response.body.daily.data[0].summary + ' It is currently ' , response.body.currently.temperature , ' degrees out. There is a ', response.body.currently.precipProbability , ' percent chance of rain' )
+//     }
+//     //    const data = JSON.parse(response.body)
+//     //    console.log(data.currently)
+//     })
+
+// const geoCodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoieWFzaXJ0aGVkdXJyYW5pIiwiYSI6ImNqdnhoMXA5azA1MmY0YW1qaDRjZjluMnIifQ.JC-I3GJHfksjPee6Q_19IA&limit=1'
 
 
-request({url:geoCodeurl, json: true},(error, response) => {
+// request({url:geoCodeurl, json: true},(error, response) => {
 
-    if(error){
-        console.log('Cannot connect to the locator')
-    }else if(response.body.features.length === 0){
-        console.log('Cannot find the location')
-    }else{
+//     if(error){
+//         console.log('Cannot connect to the locator')
+//     }else if(response.body.features.length === 0){
+//         console.log('Cannot find the location')
+//     }else{
 
-    const longitude = response.body.features[0].center[0]
-    const latitude = response.body.features[0].center[1]
+//     const longitude = response.body.features[0].center[0]
+//     const latitude = response.body.features[0].center[1]
 
-    console.log( 'Latitude = ' + latitude + ' Longitude = ' + longitude)
-    }
+//     console.log( 'Latitude = ' + latitude + ' Longitude = ' + longitude)
+//     }
    
     
-})
+// })
---
+++ random.js
@@ -0,0 +1,46 @@
+// // explaining  
+
+// setTimeout(() => {
+//     console.log('The time is up')
+// }, 2000);
+
+// const name = [ 'yasir', 'zaid', 'danny']
+// const shortnames = name.filter((names) => {
+//   return names.length <= 4
+// })
+
+// const geocode = (address, callback)  => {
+//     setTimeout(() => {
+    
+//         const data = {
+//             longitude: 0,
+//             latitude: 0,
+//     }
+
+//         callback (data)
+    
+//         }, 2200);
+
+        
+// }
+
+// geocode('Toronto', (dateeea) =>{
+//     console.log(dateeea)
+
+// })
+
+// // Define an add function that accepts the correct arguments
+// // 2. Use setTimeout to simulate a 2 second delay
+// // 3. After 2 seconds are up, call the callback function with the sum
+// // 4. Test your work!
+
+// const add = ( a, b, callback) =>{
+//     setTimeout(() => {
+        
+//         callback(a+b)
+//     }, 2000);
+// }
+
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
