b919424237f9747df0e1426338aa465fe9c76079
Yasir Durrani <yudurrani@gmail.com>
2019-05-30 18:40:35 -0400

    adding help and help

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,24 +5,10 @@ const express = require ('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res)=>{
-    res.send('<h1>Weather</h1>')
-})
 
-app.get('/help', (req, res) =>{
-    res.send([{
-        name:'Yasir',
-    },{
-        name: 'Sarah',
-        age:32
-    }])
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
-})
 
 app.get('/weather', (req, res) => {
     res.send([{
