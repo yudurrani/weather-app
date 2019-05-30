8ae0e30dd96a82c2a730bd97a3701fad0d46e325
Yasir Durrani <yudurrani@gmail.com>
2019-05-30 17:44:18 -0400

    adding a static file

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,11 @@
+const path = require('path')
 const express = require ('express')
 
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res)=>{
     res.send('<h1>Weather</h1>')
@@ -28,8 +33,6 @@ app.get('/weather', (req, res) => {
     }])
 })
 
-// app.com
-// app.com/help
 
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000')
