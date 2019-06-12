a9625650e9a77c5847f297519fe7f8c82639e6ba
Yasir Durrani <yudurrani@gmail.com>
2019-06-12 18:56:16 -0400

    adding templates for Dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,31 @@ const express = require ('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
  
 app.set('view engine','hbs')
+app.set('views', viewsPath)
 app.use(express.static(publicDirectoryPath))
 
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Yasir Durrani'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title:'about the app',
+        name:'this one is by me'
+        
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help')
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
