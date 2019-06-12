8c9232c72425c2094a8ff82a381ea210fec9d3ab
Yasir Durrani <yudurrani@gmail.com>
2019-06-12 17:40:40 -0400

    making hbs with other updates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,10 @@
 const path = require('path')
 const express = require ('express')
 
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
  
-
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
 
 
