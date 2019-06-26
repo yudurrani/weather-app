e57fdd761f98361691ec179c3c38572534562ea2
Yasir Durrani <yudurrani@gmail.com>
2019-06-26 01:42:25 -0400

    web server updates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -50,18 +50,25 @@ app.get('/weather', (req, res) => {
     })
     }
 
-    geocode(req.query.address, (error, {latitude, longitude, location})=>{
+    geocode(req.query.address, (error, {latitude, longitude, location} = {})=>{
         if(error){
             return res.send({error :error})
         }
+        forcast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+
+            res.send({
+                forcast: forecastData,
+                location,
+                address: req.query.address,
+            })
+        })
     })
 
-    // res.send({
-    //   forcast :
-    //   location : geoCode(req.query.address)
-    //   address : req.query.address
-        
-    // })
+    
+   
 
 })
 
