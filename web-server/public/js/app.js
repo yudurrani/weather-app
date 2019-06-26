c020267a36dc83674a58d3fc7699e65199ca5a61
Yasir Durrani <yudurrani@gmail.com>
2019-06-26 02:43:51 -0400

    linking location to the app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,35 @@
 console.log('Client side javascript file is loaded ')
+
+fetch('http://localhost:3000/weather?address=!').then((response) => {
+    response.json().then((data) =>{ 
+        if (data.error){
+            console.log(data.error)
+        }
+        else {
+            console.log(data.location)
+            console.log(data.forcast)
+        }
+    })
+})
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location= search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((locationw) =>{ 
+        if (locationw.error){
+            console.log(locationw.error)
+        }
+        else {
+            console.log(locationw.location)
+            console.log(locationw.forcast)
+        }
+    })
+})
+    
+})
