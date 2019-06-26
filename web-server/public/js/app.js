1baeb369678fbe2e9610ad03b0a87465da7c49c9
Yasir Durrani <yudurrani@gmail.com>
2019-06-26 14:28:49 -0400

    showing the forcast on weather page

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,33 +1,29 @@
 console.log('Client side javascript file is loaded ')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) =>{ 
-        if (data.error){
-            console.log(data.error)
-        }
-        else {
-            console.log(data.location)
-            console.log(data.forcast)
-        }
-    })
-})
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherform.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location= search.value
+    
+
+    messageOne.textContent ='Loading...'
+    messageTwo.textContent = ''
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-    response.json().then((locationw) =>{ 
-        if (locationw.error){
-            console.log(locationw.error)
+    response.json().then((data) =>{ 
+        if (data.error){
+            messageOne.textContent = data.error
         }
         else {
-            console.log(locationw.location)
-            console.log(locationw.forcast)
+            messageOne.textContent= data.location
+            messageTwo.textContent = data.forecast
+   
         }
     })
 })
